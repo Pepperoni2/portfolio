89afdf3ce70a6a0051c01ab8ddbe4e06c171ad5b
Florian Golubic <florian.golubic@gmail.com>
2025-01-12 13:03:03 +0100

    added navbar
    working on responsivness

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', nav)
+function nav(){
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navbar');
+    const main = document.querySelector('main')
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('show');
+        main.classList.toggle('darken');
+    })
+}
